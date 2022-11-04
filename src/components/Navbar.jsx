f7e4b92004e940a143eea00ef89629492b573890
rifkit <rieyfkiey@gmail.com>
2022-11-04 17:28:40 +0700

    navbar selesai

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 const Navbar = () => {
+    
+    const menuAction = () => {  
+        document.querySelector('.menu').classList.toggle('hidden')
+    }
+
     return(
-        <nav className="px-[100px] flex justify-between items-center bg-slate-100 h-[70px]">
+        <nav className="relative px-[20px] md:static md:px-[50px] lg:px-[100px] flex justify-between items-center bg-slate-100 h-[70px]">
             <div className="nav-brand">
                 <a href="#" className="text-lg font-bold">ReactFilm</a>
             </div>
-            <ul className="h-full flex items-center justify-between">
-                <li><a href="#" className="px-5 py-3 rounded hover:bg-slate-200">Trending</a></li>
-                <li><a href="#" className="px-5 py-3 rounded hover:bg-slate-200">Home</a></li>
-                <li><a href="#" className="px-5 py-3 rounded hover:bg-slate-200">Latest</a></li>
-                <li><a href="#" className="px-5 py-3 rounded hover:bg-slate-200">Contact</a></li>
+            <button className="md:hidden" onClick={menuAction}>
+                <i className="bi bi-list flex justify-center items-center p-2 rounded text-lg bg-slate-200 hover:bg-slate-300"></i>
+            </button>
+            <ul className="menu hidden w-[200px] md:w-min p-2 absolute right-5 top-[75px] text-center bg-slate-100 
+                md:static md:bg-transparent md:p-0 md:items-center md:justify-between md:flex">
+                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Trending</a></li>
+                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Home</a></li>
+                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Latest</a></li>
+                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Contact</a></li>
             </ul>
         </nav>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
+import '/node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import './index.css';
 import Navbar from './components/Navbar';
 import reportWebVitals from './reportWebVitals';
