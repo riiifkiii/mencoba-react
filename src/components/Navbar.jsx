29978f39d088a3ea0bf049358017740f81426807
riiifkiii <rieyfkiey@gmail.com>
2022-11-09 19:07:17 +0700

    mengubah navbar, dan membuat navbar baru versi mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,38 @@
-const Navbar = () => {
-    
-    const menuAction = () => {  
-        document.querySelector('.menu').classList.toggle('hidden')
+import React from 'react'
+import {CiSliderHorizontal, CiSearch, CiMedicalCross} from 'react-icons/ci'
+
+export default function Navbar() {
+
+    const menu = () => {
+        document.querySelector('.menu-link').classList.toggle('hidden')
     }
 
-    return(
-        <nav className="relative px-[20px] md:static md:px-[50px] lg:px-[100px] flex justify-between items-center bg-slate-100 h-[70px]">
-            <div className="nav-brand">
-                <a href="#" className="text-lg font-bold">ReactFilm</a>
-            </div>
-            <button className="md:hidden" onClick={menuAction}>
-                <i className="bi bi-list flex justify-center items-center p-2 rounded text-lg bg-slate-200 hover:bg-slate-300"></i>
-            </button>
-            <ul className="menu hidden z-10 w-[200px] md:w-min p-2 absolute right-5 top-[75px] text-center bg-slate-100 
-                md:static md:bg-transparent md:p-0 md:items-center md:justify-between md:flex">
-                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Trending</a></li>
-                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Home</a></li>
-                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Latest</a></li>
-                <li className=""><a href="#" className="block w-full py-3 md:px-5 md:py-3 rounded hover:bg-slate-300">Contact</a></li>
-            </ul>
-        </nav>
-    )
-}
+    const search = () => {
+        document.querySelector('.search-screen').classList.toggle('hidden')
+    }
 
-export default Navbar
+  return (
+    <nav className='px-5 py-3 bg-slate-900 flex items-center justify-between gap-2'>
+        <div className="navbar-brand flex-1">
+            <a href="" className='text-white text-lg font-bold'>Movies<span className=' text-red-500'>DB</span></a>
+        </div>
+        <div className="menu relative">
+            <button className='flex items-center justify-center border border-white w-8 h-8 text-xl text-white rounded' onClick={menu}><CiSliderHorizontal /></button>
+            <ul className='menu-link hidden absolute right-0 top-10 bg-white rounded p-2 w-[300px] text-center'>
+                <li><a href="" className='block hover:bg-red-500 hover:text-white w-full h-full p-2 rounded'>Movies</a></li>
+                <li><a href="" className='block hover:bg-red-500 hover:text-white w-full h-full p-2 rounded'>Tv Series</a></li>
+                <li><a href="" className='block hover:bg-red-500 hover:text-white w-full h-full p-2 rounded'>Actors</a></li>
+                <li><a href="" className='block hover:bg-red-500 hover:text-white w-full h-full p-2 rounded'>Categroies</a></li>
+            </ul>
+        </div>
+        <div className="search-bar">
+            <button className='search flex items-center justify-center bg-red-500 hover:bg-red-600 w-8 h-8 text-xl text-white rounded' onClick={search}><CiSearch /></button>
+            <div className="search-screen hidden absolute top-0 left-0 px-5 bg-red-500 w-full h-full text-center pt-10">
+                <h2 className='text-white text-2xl font-bold uppercase mb-5'>Search</h2>
+                <input type="text" className='rounded h-10 w-[80%] text-center' placeholder='Movies, Tv Show and Actors'/>
+                <button className="close absolute right-5 top-3 text-xl text-white hover:border border-white w-8 h-8 rounded flex items-center justify-center" onClick={search}><CiMedicalCross className='rotate-[45deg]' /></button>
+            </div>
+        </div>
+    </nav>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
 import '/node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import './index.css';
 import Navbar from './components/Navbar';
 import reportWebVitals from './reportWebVitals';
-import Hero from './components/Hero';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <Navbar />
-    <Hero />
   </React.StrictMode>
 );
 
