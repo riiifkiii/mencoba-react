b8e18f3a0dfc5b890cbcf4bb805ecdb28cb9f2b4
riiifkiii <rieyfkiey@gmail.com>
2022-11-13 17:14:42 +0700

    api untuk slider selesai

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {Pagination, Autoplay} from 'swiper'
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay'
 import SwiperItem from './SwiperItem';
-import { useEffect } from 'react';
-import { useState } from 'react';
 
 export default () => {
-
-    const omdbapi = `http://www.omdbapi.com?apikey=c032e2d7&s=naruto`
-    const tmdb = `https://api.themoviedb.org/3/trending/movie/week?api_key=15e383204c1b8a09dbfaaa4c01ed7e17`
+    const TMDB_API = `https://api.themoviedb.org/3/trending/movie/week?api_key=15e383204c1b8a09dbfaaa4c01ed7e17`
 
     const Movies = async () => {
-        const response = await fetch(tmdb)
-        const data = await response.json()
-
-        setMovies(data.results)
-        // setMovies(data.Search)
-        // console.log(data.Search)
+        await fetch(TMDB_API)
+            .then(response => {
+                return response.json()
+            })
+            .then( data => {
+                setMovies(data.results)
+            })
     }
 
     const [movies, setMovies] = useState([])
@@ -32,53 +29,22 @@ export default () => {
         <Swiper
         modules={[Pagination, Autoplay]}
         loop={true}
-        autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
-        }}
+        // autoplay={{
+        //     delay: 5000,
+        //     disableOnInteraction: false,
+        // }}
         pagination={{ clickable: true }}
         // onSlideChange={() => console.log('slide change')}
         // onSwiper={(swiper) => console.log(swiper)}
         className="h-[450px]"
         >
             {
-                movies.map((movie) => {
-                    {console.log(movie.title)}
-                    return <SwiperSlide className='relative flex items-center justify-between text-white text-center md:block md:text-left'>
+                movies.slice(0,5).map((movie) => {
+                    return <SwiperSlide key={movie.id} className='relative flex items-center justify-between text-white text-center md:block md:text-left'>
                         <SwiperItem data={movie} />
-                        {console.log(movie)}
                     </SwiperSlide>
                 })
             }
-
-
-            {/* <SwiperSlide className='relative flex items-center justify-between text-white text-center md:block md:text-left'>
-                <div className="caption absolute bottom-0 w-full bg-slate-900 h-[300px] md:top-1/2 md:-translate-y-1/2 md:h-full md:w-[600px] md:flex md:flex-col md:justify-center md:ml-10 lg:ml-16 md:bg-transparent">
-                    <div className="title my-5">
-                        <h1 className='text-2xl md:text-4xl font-bold mb-1'>sdfsdf</h1>
-                        <p className='text-xs'>
-                            <span>16 September 2012</span>
-                            <span className='mx-3'></span>
-                            <span>8.9/10</span>
-                        </p>
-                    </div>
-                    <div className="trailer w-full">
-                        <iframe src="https://www.youtube.com/embed/iVAgTiBrrDA" className='mx-auto md:m-0' title="The Hobbit: The Battle of the Five Armies - Official Main Trailer [HD]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                </div>
-                <div className="poster absolute top-0 h-[200px] w-full -z-10 md:h-full md:w-[800px] md:right-0 md:before:absolute md:before:top-0 md:before:left-0 md:before:h-full md:before:w-full md:before:bg-gradient-to-r md:before:from-slate-900 md:before:to-transparent md:before:z-20">
-                    <img src="https://wallpaperaccess.com/full/8499738.jpg" alt="" className='object-cover h-full w-full'/>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://free4kwallpapers.com/uploads/wallpaper/demon-slayer-wallpaper-1600x900-wallpaper.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://www.hdwallpapers.in/download/giyuu_tomioka_inosuke_hashibira_kanao_tsuyuri_kyojuro_rengoku_nezuko_kamado_shinobu_kochou_tanjirou_kamado_zenitsu_agatsuma_4k_5k_hd_demon_slayer_kimetsu_no_yaiba-1600x900.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="https://wallpapercave.com/wp/wp6750306.jpg" alt="" />
-            </SwiperSlide> */}
         </Swiper>
     );
 };
--- src/components/SwiperItem.jsx
+++ src/components/SwiperItem.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function SwiperItem(props) {
-    console.log(props)
+export default function SwiperItem({data}) {
+
+    const TMDB_API_MOVIES_DETAIL = `https://api.themoviedb.org/3/movie/${data.id}?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&language=en-US`
+    const TMDB_API_MOVIES_TRAILLER = `https://api.themoviedb.org/3/movie/${data.id}/videos?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&language=en-US`
+
+    const DetailMovies = async () => {
+        await fetch(TMDB_API_MOVIES_DETAIL)
+            .then( response => {
+                return response.json()
+            })
+            .then( data => {
+                // console.log(data)
+                setDetailMovie(data)
+            })
+    }
+
+    const Trailler = async () => {
+        await fetch(TMDB_API_MOVIES_TRAILLER)
+            .then( response => {
+                return response.json()
+            })
+            .then( data => {
+                const m = data.results.slice(0, 1).map( detail => {
+                    // return console.log(detail)
+                    setTrailler(detail)
+                })
+            })
+    }
+
+    const [detailMovie, setDetailMovie] = useState([])
+    const [trailler, setTrailler] = useState([])
+    
+    useEffect(() => {
+        DetailMovies()
+        Trailler();
+    },[])
+
+    console.log(trailler.key)
+
   return (
     <>
     <div className="caption absolute bottom-0 w-full bg-slate-900 h-[300px] md:top-1/2 md:-translate-y-1/2 md:h-full md:w-[600px] md:flex md:flex-col md:justify-center md:ml-10 lg:ml-16 md:bg-transparent">
         <div className="title my-5">
-            <h1 className='text-2xl md:text-4xl font-bold mb-1'>{props.data.title}</h1>
+            <h1 className='text-2xl md:text-4xl font-bold mb-1'>{detailMovie.original_title}</h1>
             <p className='text-xs'>
-                <span>{props.data.release_date}</span>
+                <span>{detailMovie.release_date}</span>
                 <span className='mx-3'></span>
-                <span>8.9/10</span>
+                <span>{detailMovie.vote_average}/10</span>
             </p>
         </div>
         <div className="trailer w-full">
-            {/* <iframe src="https://www.youtube.com/embed/iVAgTiBrrDA" className='mx-auto md:m-0' title="The Hobbit: The Battle of the Five Armies - Official Main Trailer [HD]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+            <iframe src={"https://www.youtube.com/embed/"+trailler.key} className='mx-auto md:m-0' title="The Hobbit: The Battle of the Five Armies - Official Main Trailer [HD]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
     <div className="poster absolute top-0 h-[200px] w-full -z-10 md:h-full md:w-[800px] md:right-0 md:before:absolute md:before:top-0 md:before:left-0 md:before:h-full md:before:w-full md:before:bg-gradient-to-r md:before:from-slate-900 md:before:to-transparent md:before:z-20">
-        <img src={"https://image.tmdb.org/t/p/original/"+props.data.backdrop_path} alt="" className='object-cover h-full w-full'/>
+        <img src={"https://image.tmdb.org/t/p/original/"+detailMovie.backdrop_path} alt="" className='object-cover h-full w-full'/>
     </div>
     </>
   )
